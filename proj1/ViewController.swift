1c120c880400b099b35616bf027f23484c912be0
sakamotoyuya <will-wish@docomo.ne.jp>
2019-03-26 00:06:38 +0900

    Initial Commit

---
+++ proj1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  proj1
+//
+//  Created by sakamotoyuya on 2019/03/25.
+//  Copyright © 2019 sakamotoyuya. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
