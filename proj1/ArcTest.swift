df362e3595e141da86486d2902ea5f217ed014a2
sakamotoyuya <will-wish@docomo.ne.jp>
2019-04-02 01:33:01 +0900

    ArcTestクラスを追加し、ViewControllerからARCのテストが行得るように修正しました。

---
+++ proj1/ArcTest.swift
@@ -0,0 +1,36 @@
+//
+//  ArcTest.swift
+//  proj1
+//
+//  Created by sakamotoyuya on 2019/03/31.
+//  Copyright © 2019 sakamotoyuya. All rights reserved.
+//
+
+import UIKit
+
+class ArcTest: NSObject {
+    var a: A? = A()
+    var b: B? = B()
+    /// コンストラクタ
+    override init(){
+        super.init()
+        a!.b = b
+        b!.a = a
+    }
+}
+
+class A {
+    var b: B?
+    func call(){
+        print("A call")
+    }
+    deinit { print("Aを解放しました") }
+}
+
+class B {
+    weak var a: A?
+    func call(){
+        print("B call")
+    }
+    deinit { print("Bを解放しました") }
+}
